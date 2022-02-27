bf63b1157d9b965f84c16bf401c95f574b65f660
Jeff <iambansah@gmail.com>
2022-02-27 20:10:11 +0000

    login with strapi

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -17,98 +17,81 @@ function CartPage () {
     
     return (
         <Wrapper>
-            <div className="banner">
-                <Container>
-                    <h1> Cart </h1>
-                </Container>
-            </div>   
             <Container>
-                <section className="section">
-                <table>
-                    <tbody>
-                    <tr>
-                        <th>Item</th>
-                        <th>Price</th>
-                        <th>Quantity</th>
-                        <th>Sub Total</th>
-                        <th></th>
-                    </tr>
-                    { cart.map((item)=> {
-                        return (
-                        <tr key={item.images[0]['url']}>
-                            <td>
-                                <p> {item.name}  </p>
-                                <img src={ item.images[0]['url'] } alt="" />
-                            </td>
-                            <td> $ {item.price / 100} </td>
-                            <td>
-                                <div className="counter">                    
-                                    <span className="counts" onClick = { ()=>decreaseCart( item ) } > - </span> &nbsp; { item.amount } &nbsp; <span className="counts" onClick = { ()=>increaseCart( item )} > + </span>
-                                </div>                                
-                            </td>
-                            <td> { (item.price * item.amount) / 100 } </td>
-                            <td onClick={ ()=>delPro(item) }>Delete</td>
-                        </tr>
-                        )
-                    }) }
-                </tbody>                    
-                </table>   
-                </section> 
+                <div className="form">
+                    <p>LOGIN </p>
+                    <input 
+                    type="text" 
+                    name="email"        
+                    placeholder="email"                            
+                    />
 
-                <button onClick={ clearCart }> clear cart</button>
-                <p> Sub total : { subTotal } </p>                 
-                <p> Tax : { tax } </p>
-                <p> total : { total }  </p>
+                    <input 
+                    type="password" 
+                    name="email"        
+                    placeholder="password"                            
+                    />         
 
-                <Link to="/products"> Go back and shop </Link>
+                    <button 
+                    type="submit"
+
+                    > SUBMIT </button>           
+                </div>
+
+                <div className="form">
+                    <p>REGISTER </p>
+                    <input 
+                    type="text" 
+                    name="email"        
+                    placeholder="email"                            
+                    />
+
+                    <input 
+                    type="password" 
+                    name="email"        
+                    placeholder="password"                            
+                    />       
+
+                    <input 
+                    type="text" 
+                    name="email"        
+                    placeholder="username"                            
+                    />                        
+
+                    <button 
+                    type="submit"
+
+                    > SUBMIT </button>           
+                </div>                
             </Container>
         </Wrapper>
     )
 }
 
 const Wrapper = styled.div`
-    .banner {
-        background-color: #eaded7;
-        height: 150px;
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        font-family: helvetica;
-        color: #453227;        
-    }
-
-    .section {
-        display: flex;
-        justify-content: space-between;
-        margin-top: 2rem;
-    }
-
-    table {
-        font-family: arial, sans-serif;
-        border-collapse: collapse;
-        width: 100%;
-
-        img {
-            height: 80px;
-            width: 110px;
-        }
-    }
-
-    td, th {
-        border: 1px solid #dddddd;
-        text-align: left;
-        padding: 8px;
-        
-    }
-
-    tr:nth-child(even) {
-        background-color: #dddddd;
-    }    
 `
 
 const Container = styled.div`
         max-width: 90%;
         margin: 0 auto;
+
+        .form {
+            margin: 0 auto;
+            width: 70%;
+            background-color: whitesmoke;
+            margin-top: 3rem;
+            padding: 2rem;
+
+            p {
+                text-align: center;
+            }
+            input , button{
+                margin: 0 auto;
+                display: block;
+                padding: 1rem;
+                outline: none;
+            }
+        }
 `
 
 
